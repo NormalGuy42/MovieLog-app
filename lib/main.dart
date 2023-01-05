9c925dfc06a517e859977dca1bb5d434215381da
M1000 <76665071+NormalGuy42@users.noreply.github.com>
2023-01-05 17:28:27 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'pages/MyHomePage.dart';
+import 'pages/AddMovie.dart';
+import 'pages/MyProfile.dart';
 
+void main() {
+  runApp(DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MaterialApp(
+            useInheritedMediaQuery: true,
+            debugShowCheckedModeBanner: false,
+            home: MyApp(),
+          )));
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  final screens = [
+    MyHomePage(),
+    AddMovie(),
+    MyProfile(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: IndexedStack(index: _currentIndex, children: screens),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add Movie'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+        ],
+      ),
+    );
+  }
+}
