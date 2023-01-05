532c62922e96434d986f43fd5adc9d3e9dd5f4e6
M1000 <76665071+NormalGuy42@users.noreply.github.com>
2023-01-05 17:29:26 +0000

    Update MyHomePage.dart

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text('Home', style: TextStyle(color: Colors.white)),
+    ));
+  }
+}
